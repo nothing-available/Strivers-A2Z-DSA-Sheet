2069daee3e030d4b16c70687a68250206cf733d6
Sumit kumar <78586294+nothing-available@users.noreply.github.com>
2023-01-21 02:37:10 +0530

    Create 704. Binary Search.cpp

---
+++ Binary Search/Learning BS on 1D Arrays/704. Binary Search.cpp
@@ -0,0 +1,32 @@
+// Expected Time Complexity: O(LogN)
+// Expected Auxiliary Space: O(LogN) if solving recursively and O(1) otherwise.
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
+            if( target == nums[mid]){
+
+                return mid;
+            }
+
+            else if(target < nums[mid]){
+
+                end = mid - 1;
+            }
+
+            else{
+
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
