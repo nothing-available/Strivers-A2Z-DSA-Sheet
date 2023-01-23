650e5de918183c351b1ca9aa44e8597ced8c0044
Sumit kumar <78586294+nothing-available@users.noreply.github.com>
2023-01-24 02:36:42 +0530

    Create Print GFG n times.cpp

---
+++ Learn the basic/Learn basic recurssion/Print GFG n times.cpp
@@ -0,0 +1,15 @@
+//Expected Time Complexity: O(N).
+//Expected Auxiliary Space: O(N) (Recursive).
+
+class Solution {
+  public:
+    void printGfg(int N) {
+        // Code here
+        if(N == 0){
+            return;
+        }
+        cout<<"GFG"<<" ";
+        printGfg(N-1);
+        
+    }
+};
