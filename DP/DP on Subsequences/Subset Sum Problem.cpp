2a9d047b534a4bcf85ece9799be90af6119c6935
nothing-available <smtkur31@gmail.com>
2024-02-05 13:38:48 +0530

    add

--- DP/DP on Subsequences/Subset Sum Problem.cpp
+++ DP/DP on Subsequences/Subset Sum Problem.cpp
@@ -36,3 +36,42 @@ public:
 
 //memoization soln.
 
+class Solution{   
+public:
+
+    bool isSubsetSum(vector<int>arr, int sum){
+        
+        int n = arr.size();
+        
+        vector<vector<int>>dp (n+1, vector<int>(sum+1, -1));
+        
+        return subsetSum(arr, n, sum, dp);
+    }
+    
+    bool subsetSum(vector<int> arr, int n, int sum, vector<vector<int>>& dp){
+        
+        //base condition
+        
+        if(sum == 0) return true;
+        
+        if(n == 0) return false;
+        
+        if(dp[n-1][sum] != -1) return dp[n-1][sum];
+        
+        if(arr[n-1] <= sum){
+            
+            return dp[n-1][sum] = subsetSum(arr, n-1, sum - arr[n-1], dp) or subsetSum(arr, n-1, sum, dp);
+        }
+        
+        else{
+            
+            return dp[n-1][sum] = subsetSum(arr, n-1, sum, dp);
+        }
+            
+    }
+    
+};
+
+//tabulation soln.
+
+
